88caf6cdfe1bf4fd8d994640522f73737a7baff4
stillgood96 <a787574@naver.com>
2021-09-05 19:52:09 +0900

    Object Mapper

--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.hello;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HelloApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("------------");
+        System.out.println("hello");
+
+
+        // Object Mappper
+        // Object -> Text JSON
+
+        // controller req json(text) -> object
+        // response oject -> json(text)
+
+        var objectMapper = new ObjectMapper();
+
+
+        // object -> text
+        // object mapper get method 를 활용한다.
+        var user = new User("sunwoo", 15);
+
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        // text -> object
+        // object mapper는 default 생성자를 필요로 한다.
+        // + object mapper가 참조하는 클래스의 경우 get method를 참조하기 때문에
+        // 다른 메서드에 get이 존재하면 안된다.
+        var objectUser = objectMapper.readValue(text, User.class);
+        System.out.println(objectUser);
+     }
 
 }
---
+++ src/test/java/com/example/hello/User.java
@@ -0,0 +1,43 @@
+package com.example.hello;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+    private String name;
+    private int age;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public User() {
+        this.name=null;
+        this.age= 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
