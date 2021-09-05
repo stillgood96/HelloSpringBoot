2ad7883e75aac9a10a6238679ef8d4333eefa4a4
a7875 <a787574@naver.com>
2021-09-05 15:41:17 +0900

    put api

---
+++ src/main/java/com/example/hello/dto/CarDto.java
@@ -0,0 +1,33 @@
+package com.example.hello.dto;
+
+
+public class CarDto {
+
+    private String name;
+    private String carNumber;
+
+
+    @Override
+    public String toString() {
+        return "CarDto{" +
+                "name='" + name + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+}
